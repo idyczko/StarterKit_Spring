b50d1e29629bddb56d068f9043514755a7405278
Artur Otrzonsek <artur.otrzonsek@capgemini.com>
2015-07-03 07:35:54 +0200

    save book service method added

--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
@@ -3,6 +3,7 @@ package pl.spring.demo.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.spring.demo.entity.BookEntity;
 import pl.spring.demo.mapper.BookMapper;
 import pl.spring.demo.repository.BookRepository;
 import pl.spring.demo.service.BookService;
@@ -35,6 +36,8 @@ public class BookServiceImpl implements BookService {
     @Override
     @Transactional(readOnly = false)
     public BookTo saveBook(BookTo book) {
-        return null;
+        BookEntity entity = BookMapper.map(book);
+        entity = bookRepository.save(entity);
+        return BookMapper.map(entity);
     }
 }
