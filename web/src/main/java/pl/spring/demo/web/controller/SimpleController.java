2b9e3c45bbdbc6bf2f440f4daffdfd56395faad7
Artur Otrzonsek <artur.otrzonsek@capgemini.com>
2015-07-15 21:25:58 +0200

    logback configuration changed

--- web/src/main/java/pl/spring/demo/web/controller/SimpleController.java
+++ web/src/main/java/pl/spring/demo/web/controller/SimpleController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Controller
 public class SimpleController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String home(Map<String, Object> parameters) {
         parameters.put("book", new BookTo(1L, "First Book Title", null));
         parameters.put("booksCount", 1);
