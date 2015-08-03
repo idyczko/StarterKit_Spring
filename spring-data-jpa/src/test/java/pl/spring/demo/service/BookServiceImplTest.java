619fad345007b0ed2627b516f8f5856db2fc4cec
idyczko <igor.dyczko@gmail.com>
2015-08-03 10:53:34 +0200

    Unused imports deleted

--- spring-data-jpa/src/test/java/pl/spring/demo/service/BookServiceImplTest.java
+++ spring-data-jpa/src/test/java/pl/spring/demo/service/BookServiceImplTest.java
@@ -2,7 +2,6 @@ package pl.spring.demo.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
